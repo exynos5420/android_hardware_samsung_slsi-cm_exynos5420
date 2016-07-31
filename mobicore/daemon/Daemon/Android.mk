2658067695b4389b5a1a6bb0c99969165cf6bfa0
Christopher N. Hesse <raymanfx@gmail.com>
2016-07-31 14:41:36 +0200

    mobicore: Silence date/time warnings
    Change-Id: I8c63c0f8e212098d77d1915eb27b18a32af6ed95
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- mobicore/daemon/Daemon/Android.mk
+++ mobicore/daemon/Daemon/Android.mk
@@ -8,7 +8,10 @@
 LOCAL_SRC_FILES += Daemon/MobiCoreDriverDaemon.cpp
 
 # Includes required for the Daemon
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public
+
+# Ignore non-reproducible date/time expansions
+LOCAL_CFLAGS += -Wno-date-time
 
 # Internal components
 include $(LOCAL_PATH)/Daemon/Device/Android.mk
