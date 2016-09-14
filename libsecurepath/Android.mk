CyanogenMod/android_hardware_samsung_slsi-cm_exynos5433

mobicore: Purge makefiles
Many of these include paths are non-existant and many makefile
rules are actually broken.
All of them will be errors in N, so fix it before it bites us.

Change-Id: Iceb5a49879fb287f8f1cb9d80799d9e6cec34ce5

libsecurepath/Android.mk
@@ -31,10 +31,11 @@ LOCAL_SRC_FILES	+= 	\
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../../exynos5/include \
-	$(TOP)/hardware/samsung_slsi-cm/exynos/include
+	$(TOP)/hardware/samsung_slsi-cm/exynos/include \
+	$(TOP)/hardware/samsung_slsi-cm/exynos5433/mobicore/common/LogWrapper
 
-LOCAL_SHARED_LIBRARIES += libMcClient
+LOCAL_SHARED_LIBRARIES += libMcClient liblog
 
-include hardware/samsung_slsi-cm/$(TARGET_SOC)/mobicore/common/LogWrapper/Android.mk
+LOCAL_CFLAGS += -DLOG_ANDROID
 
 include $(BUILD_STATIC_LIBRARY)

exynos5420/android_hardware_samsung_slsi-cm_exynos5420

Signed-off-by: Paul Keith <javelinanddart@gmail.com>

libsecurepath/Android.mk
@@ -31,10 +31,11 @@ LOCAL_SRC_FILES	+= 	\
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../../exynos5/include \
-	$(TOP)/hardware/samsung_slsi-cm/exynos/include
+	$(TOP)/hardware/samsung_slsi-cm/exynos/include \
+	$(TOP)/hardware/samsung_slsi-cm/exynos5420/mobicore/common/LogWrapper
 
-LOCAL_SHARED_LIBRARIES += libMcClient
+LOCAL_SHARED_LIBRARIES += libMcClient liblog
 
-include hardware/samsung_slsi-cm/$(TARGET_SOC)/mobicore/common/LogWrapper/Android.mk
+LOCAL_CFLAGS += -DLOG_ANDROID
 
 include $(BUILD_STATIC_LIBRARY)