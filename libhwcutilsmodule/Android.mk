4720a60523082777b56632d681a9f85e1ac5a07a
Thomas Patry <thompatry@gmail.com>
2018-02-04 00:55:33 -0600

    Rename libdisplay to libexynosdisplay continued
    In O the vr libdisplay causes a conflict
    
    Change-Id: Idc4b12221127f966bc2bc7410379a6659c2599bc

--- libhwcutilsmodule/Android.mk
+++ libhwcutilsmodule/Android.mk
@@ -32,7 +32,7 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi-cm/$(TARGET_SOC)/libhwcmodule \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libhwc \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libhwcutils \
-	$(TOP)/hardware/samsung_slsi-cm/exynos/libdisplay \
+	$(TOP)/hardware/samsung_slsi-cm/exynos/libexynosdisplay \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libmpp
 
 LOCAL_ADDITIONAL_DEPENDENCIES += \
