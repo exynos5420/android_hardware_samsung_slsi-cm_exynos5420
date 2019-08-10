c97d6667bef93b1d5bea9ae745c0f044982648ef
Thomas Patry <thompatry@gmail.com>
2019-08-10 17:33:37 -0500

    Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Change-Id: Ia977d26b50c085fe718fd29476594d632f418ffc

--- mobicore/rootpa/Code/Android/app/Android.mk
+++ mobicore/rootpa/Code/Android/app/Android.mk
@@ -8,6 +8,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := rootpa_interface
 LOCAL_JNI_SHARED_LIBRARIES := libcommonpawrapper
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_PACKAGE_NAME := RootPA
 LOCAL_MODULE_TAGS := optional
